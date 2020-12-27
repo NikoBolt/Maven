2d00422474c70160cde9e632284f1ae94ffba870
NikoBolt <docnikbolt@gmail.com>
2020-12-27 21:30:38 +0300

    побыстрому повторить что с мавеном делать

---
+++ +++/_16_модульный_проект_2/calc-logic2/src/main/java/evguru/calc/Calculator.java
@@ -0,0 +1,7 @@
+package evguru.calc;
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ +++/_16_модульный_проект_2/calc-output/src/main/java/Printer.java
@@ -0,0 +1,10 @@
+import evguru.calc.Calculator;
+
+public class Printer {
+    public static void main(String[] args) {
+
+        int result = Calculator.add(5,7);
+
+        System.out.println(result);
+    }
+}
